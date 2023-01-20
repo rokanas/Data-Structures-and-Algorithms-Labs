b826b7dec52170dd55d73c3044fa212f69cca82c
Konstantinos Rokanas <rokanas@student.chalmers.se>
2023-01-20 13:26:51 +0100

    First submission of Lab1

--- BinarySearch.java
+++ BinarySearch.java
@@ -9,12 +9,46 @@ public class BinarySearch {
 
   // Check if the array `a` contains the given search key.
   public static <T> boolean contains(T[] a, T key, Comparator<T> comparator) {
-    throw new UnsupportedOperationException("TODO");
+    int low = 0;
+    int high = (a.length - 1);
+
+    return recursiveFunction(a, key, comparator, low, high);
+  }
+
+  public static <T> boolean recursiveFunction(T[] a, T key, Comparator<T> comparator, int low, int high) {
+    if (low <= high) {
+      int mid = (low + high) / 2;
+      int c = comparator.compare(key, a[mid]);
+      if (c == 0) {
+        return true;
+      } else if (c < 0) {
+        return recursiveFunction(a, key, comparator, low, (mid - 1));
+      } else if (c > 0) {
+        return recursiveFunction(a, key, comparator, (mid + 1), high);
+      }
+    }
+    return false;
   }
 
   // Return the *first position* of `key` in `a`, or -1 if `key` does not occur.
   public static <T> int firstIndexOf(T[] a, T key, Comparator<T> comparator) {
-    throw new UnsupportedOperationException("TODO");
+    int low = 0;
+    int high = (a.length - 1);
+    int mid;
+    int firstIndex = -1;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      int c = comparator.compare(key, a[mid]);
+      if (c == 0) {
+        firstIndex = mid;
+        high = mid - 1;
+      } else if (c > 0) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return firstIndex;
   }
 
   // Versions of the above functions that use the natural ordering of the type T.
@@ -44,5 +78,4 @@ public class BinarySearch {
     assert firstIndexOf(b, "zebra") == -1;
     assert firstIndexOf(b, "bee") == -1;
   }
-
 }
